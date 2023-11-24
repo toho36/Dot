9d8b6a92b26f49485c1933c7f8b940c304bf61e7
toho0036 <toho0036@gmail.com>
2023-11-24 18:17:12 +0100

    graphql install

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,56 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import ProTip from './ProTip';
+import { useQuery, gql } from '@apollo/client';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}.
-    </Typography>
-  );
+interface Location {
+  id: number; // or number, depending on the actual type of the 'id' property
+  name: string;
+  description: string;
+  photo: string;
+}
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+function DisplayLocations() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
+
+  
+  return  data.locations.map((location: Location) => (
+    <div key={location.id}>
+      <h3>{location.name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${location.photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{location.description}</p>
+      <br />
+    </div>
+  ));
 }
 
 export default function App() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Vite.js example in TypeScript
+          Dotidot table
         </Typography>
-        <ProTip />
-        <Copyright />
+        <DisplayLocations />
       </Box>
     </Container>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,35 @@ import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import theme from './theme';
 import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+const client = new ApolloClient({
+  uri: 'https://flyby-router-demo.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+// const client = ...
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ApolloProvider>
   </React.StrictMode>,
 );
